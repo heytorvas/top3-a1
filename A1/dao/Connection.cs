c1413ba7fb9a2d87f8769ac036dc7e8f9feb2478
Heytor Víctor <45018537+heytorvas@users.noreply.github.com>
2020-04-11 21:21:04 -0300

    Add files via upload

---
+++ A1/dao/Connection.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A1.dao
+{
+    public class Connection
+    {
+        public static SqlConnection connect()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=DESKTOP-CV43IN8\\SQLEXPRESS;Initial Catalog=a1;Integrated Security=True";
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            return con;
+        }
+
+        public static Boolean crud(SqlCommand cmd)
+        {
+            try
+            {
+                SqlConnection con = connect();
+                cmd.Connection = con;
+                cmd.ExecuteNonQuery();
+                con.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex);
+                return false;
+            }
+        }
+
+        public static SqlDataReader select(SqlCommand cmd)
+        {
+            try
+            {
+                SqlConnection con = connect();
+                cmd.Connection = con;
+                SqlDataReader dr = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                return dr;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex);
+                return null;
+            }
+        }
+
+        internal static object search(string v)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = v;
+            SqlDataReader dr = Connection.select(cmd);
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd.CommandText, Connection.connect());
+            da.Fill(dt);
+            return dt;
+        }
+    }
+}
---
+++ A1/dao/UserDAO.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using A1.model;
+using A1.dao;
+
+namespace A1.dao
+{
+    public class UserDAO
+    {
+        public User insert(User user)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "INSERT INTO user(name, cpf, email, password, telephone, type_user) output INSERTED.id values (@name, @cpf, @email, @password, @telephone, @type_user)";
+            cmd.Parameters.AddWithValue("@name", user.Name);
+            cmd.Parameters.AddWithValue("@cpf", user.Cpf);
+            cmd.Parameters.AddWithValue("@email", user.Email);
+            cmd.Parameters.AddWithValue("@password", user.Password);
+            cmd.Parameters.AddWithValue("@telephone", user.Telephone);
+            cmd.Parameters.AddWithValue("@type_user", user.TypeUser);
+            if (Connection.crud(cmd))
+                return user;
+            return null;
+        }
+
+        public User update(User user)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "UPDATE user set name = @name, cpf = @cpf, email = @email, password = @password, telephone = @telephone, type_user = @type_user, id = @id where id = @id";
+            cmd.Parameters.AddWithValue("@name", user.Name);
+            cmd.Parameters.AddWithValue("@cpf", user.Cpf);
+            cmd.Parameters.AddWithValue("@email", user.Email);
+            cmd.Parameters.AddWithValue("@password", user.Password);
+            cmd.Parameters.AddWithValue("@telephone", user.Telephone);
+            cmd.Parameters.AddWithValue("@type_user", user.TypeUser);
+            if (Connection.crud(cmd))
+                return user;
+            return null;
+        }
+
+        public void delete(User user)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "DELETE FROM user WHERE id = @id";
+            cmd.Parameters.AddWithValue("@id", user.Id);
+            Connection.crud(cmd);
+        }
+
+        public static User findById(int id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT * FROM user WHERE id = @id";
+            cmd.Parameters.AddWithValue("@id", id);
+            SqlDataReader dr = Connection.select(cmd);
+
+            User user = new User();
+            if (dr.HasRows)
+            {
+                dr.Read();
+                user.Id = (int)dr["id"];
+                user.Name = dr["name"].ToString();
+                user.Cpf = dr["cpf"].ToString();
+                user.Email = dr["password"].ToString();
+                user.Telephone = dr["telephone"].ToString();
+                // TODO cast enum: user.TypeUser = (int)dr["type_user"];
+            }
+            else
+                user = null;
+
+            return user;
+        }
+
+        public List<User> findAll()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT * FROM user";
+            SqlDataReader dr = Connection.select(cmd);
+            List<User> users = new List<User>();
+            UserDAO userDAO = new UserDAO();
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    dr.Read();
+                    User user = new User();
+                    user.Id = (int)dr["id"];
+                    user.Name = dr["name"].ToString();
+                    user.Cpf = dr["cpf"].ToString();
+                    user.Email = dr["password"].ToString();
+                    user.Telephone = dr["telephone"].ToString();
+                    // TODO cast enum: user.TypeUser = (int)dr["type_user"];
+                    users.Add(user);
+                }
+            }
+            else
+                users = null;
+
+            return users;
+        }
+
+        public static DataTable returnDataSource()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT * FROM user";
+            SqlDataReader dr = Connection.select(cmd);
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd.CommandText, Connection.connect());
+            da.Fill(dt);
+
+            return dt;
+        }
+    }
+}
